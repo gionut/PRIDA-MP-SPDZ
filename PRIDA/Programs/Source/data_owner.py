e61c695f071ef4c5a0b4035dc36d884223ffdfbc
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-12-14 11:30:12 +0000

    clean ccm and data_owner - remove last client logic

--- PRIDA/Programs/Source/data_owner.py
+++ PRIDA/Programs/Source/data_owner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-# To be copied to MP-SPDZ/ExternalIO/data_owner.py
 import sys
 
 sys.path.append('.')
@@ -10,17 +9,11 @@ from domains import *
 
 client_id = int(sys.argv[1])
 n_parties = int(sys.argv[2])
-finish = int(sys.argv[3])
 
 client = Client(['localhost'] * n_parties, 14000, client_id)
 
-for socket in client.sockets:
-    os = octetStream()
-    os.store(finish)
-    os.Send(socket)
-
 def run():
-    with open('DataOwner/input_' + str(client_id), 'r') as f:
+    with open('/workspaces/PRIDA-MP-SPDZ/PRIDA/Programs/DataOwner/input_' + str(client_id), 'r') as f:
         x = [int(line) for line in f]
     client.send_private_inputs(x)
 
