964dd532d0849ebd722f057f6a6d2340301fe92f
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-12-14 11:54:37 +0000

    modify ccm to enable receiveing clients

---
+++ PRIDA/Programs/Source/data_customer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.path.append('.')
+
+from client import *
+from domains import *
+
+client_id = int(sys.argv[1])
+n_parties = int(sys.argv[2])
+
+client = Client(['localhost'] * n_parties, 14000, client_id)
+
+
+def run():
+    print(f'Aggregation result for DC{client_id}: ', client.receive_outputs(1)[0])
+
+run()
